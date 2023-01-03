464e528e1a14435a83c721deff6ea1a489b7f5cb
Fitti <dev@fitti.io>
2023-01-03 11:26:52 +0100

    Extrace parse_config, and add error handling

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,30 @@
 use std::env;
+use std::error::Error;
 use std::fs;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
+
+    let (query, file_paths) = parse_config(&args)?;
+    println!("Searching for {} in {}...", query, file_paths.join(", "));
+
+    let contents = fs::read_to_string(&file_paths[0])?;
+
+    println!("With text:\n{}", contents);
+
+    return Ok(());
+}
+
+fn parse_config(args: &[String]) -> Result<(&String, &[String]), Box<dyn Error>> {
     let query;
     let file_paths;
 
     if args.len() > 2 {
         query = &args[1];
         file_paths = &args[2..];
-        println!("Searching for {} in {}...", query, file_paths.join(", "))
     } else {
-        panic!("Expected a regex pattern followed by one or more filenames!")
+        return Err("Expected a regex pattern followed by one or more file names".into());
     };
 
-    let contents = fs::read_to_string(&file_paths[0])
-        .expect(&format!("Could not read file {}", &file_paths[0]));
-
-    println!("With text:\n{}", contents);
+    return Ok((query, file_paths));
 }
