24db0dee3249090c878c40d12caf022153c819f3
Fitti <dev@fitti.io>
2023-01-03 11:35:18 +0100

    Use Config struct

--- src/main.rs
+++ src/main.rs
@@ -5,26 +5,36 @@ use std::fs;
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
-    let (query, file_paths) = parse_config(&args)?;
-    println!("Searching for {} in {}...", query, file_paths.join(", "));
+    let config = parse_config(&args)?;
+    println!(
+        "Searching for {} in {}...",
+        config.query,
+        config.file_paths.join(", ")
+    );
 
-    let contents = fs::read_to_string(&file_paths[0])?;
+    let contents = fs::read_to_string(&config.file_paths[0])?;
 
     println!("With text:\n{}", contents);
 
     return Ok(());
 }
 
-fn parse_config(args: &[String]) -> Result<(&String, &[String]), Box<dyn Error>> {
-    let query;
-    let file_paths;
+struct Config<'a> {
+    query: &'a String,
+    file_paths: &'a [String],
+}
+
+fn parse_config(args: &[String]) -> Result<Config, Box<dyn Error>> {
+    let config;
 
     if args.len() > 2 {
-        query = &args[1];
-        file_paths = &args[2..];
+        config = Config {
+            query: &args[1],
+            file_paths: &args[2..],
+        };
     } else {
         return Err("Expected a regex pattern followed by one or more file names".into());
     };
 
-    return Ok((query, file_paths));
+    return Ok(config);
 }
