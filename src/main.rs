6cc8d708953b0cb713033ac486619227bd65d47d
Fitti <dev@fitti.io>
2023-01-03 14:53:18 +0100

    Redirect errors to stderr

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("{}, got \"{}\"", err, args[1..].join(" "));
+        eprintln!("{}, got \"{}\"", err, args[1..].join(" "));
         process::exit(1);
     });
 
     if let Err(err) = minigrep::run(config) {
-        println!("{}", err);
+        eprintln!("{}", err);
         process::exit(1);
     }
 }
