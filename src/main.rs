fe81e46433aa00128d6cf22d7c3d2a505122fbb6
Fitti <dev@fitti.io>
2023-01-03 12:24:28 +0100

    Refactor error handling

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
 use std::env;
-use std::error::Error;
 use std::fs;
+use std::process;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args)?;
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("{}, got \"{}\"", err, args[1..].join(" "));
+        process::exit(1);
+    });
     println!(
         "Searching for {} in {}...",
         config.query,
         config.file_paths.join(", ")
     );
 
-    let contents = fs::read_to_string(&config.file_paths[0])?;
+    let contents = fs::read_to_string(&config.file_paths[0]).unwrap_or_else(|err| {
+        println!("Error reading file \"{}\\"": {}", config.file_paths[0], err);
+        process::exit(1);
+    });
 
     println!("With text:\n{}", contents);
-
-    return Ok(());
 }
 
 struct Config<'a> {
@@ -24,17 +28,16 @@ struct Config<'a> {
     file_paths: &'a [String],
 }
 
-fn parse_config(args: &[String]) -> Result<Config, Box<dyn Error>> {
-    let config;
-
-    if args.len() > 2 {
-        config = Config {
-            query: &args[1],
-            file_paths: &args[2..],
+impl<'a> Config<'a> {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() > 2 {
+            let config = Config {
+                query: &args[1],
+                file_paths: &args[2..],
+            };
+            return Ok(config);
+        } else {
+            return Err("Expected a regex pattern followed by one or more file names");
         };
-    } else {
-        return Err("Expected a regex pattern followed by one or more file names".into());
-    };
-
-    return Ok(config);
+    }
 }
