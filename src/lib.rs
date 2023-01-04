dda94b05000b9fa51373c1abd4b2fab741c8c9f3
Fitti <dev@fitti.io>
2023-01-04 16:44:19 +0100

    Add Crate documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
+//! # minigrep
+//!
+//! `minigrep` is a tiny command line tool built as an exercise from the book
+//! "The Rust Programming Language," chapter 12.
+//!
+//! ## Usage
+//!
+//! ```shell
+//! $ ls
+//! poem.txt minigrep
+//!
+//! $ cat poem.txt
+//! I'm nobody! Who are you?
+//! Are you nobody, too?
+//! Then there's a pair of us - don't tell!
+//! They'd banish us, you know.
+//!
+//! How dreary to be somebody!
+//! How public, like a frog
+//! To tell your name the livelong day
+//! To an admiring bog!
+//!
+//! $ ./minigrep to poem.txt
+//! Are you nobody, too?
+//! How dreary to be somebody!
+//!
+//! $ IGNORE_CASE=1 ./minigrep to poem.txt
+//! Are you nobody, too?
+//! How dreary to be somebody!
+//! To tell your name the livelong day
+//! To an admiring bog!
+//! ```
+
 use std::env;
 use std::error::Error;
 use std::fs;
